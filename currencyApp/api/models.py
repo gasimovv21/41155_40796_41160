2614c362a1a97927d02305a441acfbd1c0d63c96
Eltun Gasimov <gasimoweltun@gmail.com>
2025-03-26 23:28:44 +0400

    Created Transaction  model

--- currencyApp/api/models.py
+++ currencyApp/api/models.py
@@ -78,3 +78,15 @@ class UserCurrencyAccount(models.Model):
 def create_default_currency_account(sender, instance, created, **kwargs):
     if created:
         UserCurrencyAccount.objects.create(user=instance, currency_code='PLN')
+
+
+class Transaction(models.Model):
+    transaction_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    from_currency = models.CharField(max_length=3)
+    to_currency = models.CharField(max_length=3)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Transaction {self.transaction_id}: {self.user.username} ({self.from_currency} to {self.to_currency}, {self.amount})"
