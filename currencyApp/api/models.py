1e8a3f0aec799b4788bb27ff7f4609aaef5bc57d
Eltun Gasimov <gasimoweltun@gmail.com>
2025-03-26 23:25:36 +0400

    Created User model

--- currencyApp/api/models.py
+++ currencyApp/api/models.py
@@ -1,3 +1,22 @@
+import random
+import re
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15, unique=True, null=True)
+    email = models.EmailField(unique=True)
+    account_created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
