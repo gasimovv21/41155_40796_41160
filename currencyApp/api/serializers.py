51abb2674bb2459bdad27afe9b405a322e7f6440
Eltun Gasimov <gasimoweltun@gmail.com>
2025-03-31 01:47:19 +0200

    added serializer for user register and login

---
+++ currencyApp/api/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth.hashers import make_password
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'first_name', 'last_name', 'phone_number', 'email')
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])  # Хешируем пароль
+        return super().create(validated_data)
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
