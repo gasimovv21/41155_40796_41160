1d0df45f270763e92365b34bd9a95cc38f987542
Eltun Gasimov <gasimoweltun@gmail.com>
2025-03-31 02:45:55 +0200

    changed urls.py

--- currencyApp/api/urls.py
+++ currencyApp/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterView, LoginView, CheckJWTView
 
 urlpatterns = [
-    path('v1/register/', RegisterView.as_view(), name='register'),
-    path('v1/login/', LoginView.as_view(), name='login'),
-    path('v1/checkjwt/', CheckJWTView.as_view(), name='check-jwt'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('checkjwt/', CheckJWTView.as_view(), name='check-jwt'),
 ]
