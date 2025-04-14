db75b8916c70d37da14f41d0c90a701f3a8f6f9d
Eltun Gasimov <gasimoweltun@gmail.com>
2025-04-14 14:06:22 +0200

    added urls.py for UserCurrencyAccount, Transaction, AccountHistory, DepositHistory

--- currencyApp/api/urls.py
+++ currencyApp/api/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from .views import RegisterView, LoginView, CheckJWTView
+from .views import (
+    RegisterView, LoginView, getCurrencyAccountsView,
+    getCurrencyAccountView, getUserCurrencyAccountsView,
+    convertCurrency, depositToAccount, getAccountHistory,
+)
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('currency-accounts/', getCurrencyAccountsView, name='currency-account-list'),
+    path('currency-accounts/<int:pk>/', getCurrencyAccountView, name='currency-account-detail'),
+    path('currency-accounts/user/<int:user_id>/', getUserCurrencyAccountsView, name='user-currency-accounts'),
+
+    path('currency-accounts/convert/<int:user_id>/', convertCurrency, name='convert-currency'),
+    path('currency-accounts/deposit/<int:user_id>/', depositToAccount, name='deposit-to-account'),
+    path('currency-accounts/history/<int:user_id>/', getAccountHistory, name='account-history'),
     # path('checkjwt/', CheckJWTView.as_view(), name='check-jwt'),
 ]
